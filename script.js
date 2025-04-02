216199093ab9e0c93e97f787d89091a9733a637b
safety-crew <153456442+safety-crew@users.noreply.github.com>
2025-04-02 14:10:18 +0300

    Add Event Listener for Check Button

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+// Event Listener on '.check' button
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  // When there is no input
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No number!';
+  }
+});
